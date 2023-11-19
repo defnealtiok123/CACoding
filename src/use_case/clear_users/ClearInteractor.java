08d3f69b331c47e907385386d0ad4cdbec690731
defnealtiok123 <defnealtiok1983@gmail.com>
2023-11-19 15:48:11 -0500

    ClearInteractor Implementation

--- src/use_case/clear_users/ClearInteractor.java
+++ src/use_case/clear_users/ClearInteractor.java
@@ -2,5 +2,18 @@ package use_case.clear_users;
 
 // TODO Complete me
 
-public class ClearInteractor {
+public class ClearInteractor implements ClearInputBoundary {
+    final ClearUserDataAccessInterface userDataAccessObject;
+
+    public ClearInteractor(ClearUserDataAccessInterface clearDataAccessInterface) {
+        this.userDataAccessObject = clearDataAccessInterface;
+    }
+
+
+    //@Override
+    public void execute(ClearInputData clearInputData) {
+        userDataAccessObject.getUsers();
+        userDataAccessObject.clear();
+
+    }
 }
