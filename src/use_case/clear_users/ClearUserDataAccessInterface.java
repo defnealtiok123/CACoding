f1e32da77fcec881ff2c31bd0265c70039bdacaf
defnealtiok123 <defnealtiok1983@gmail.com>
2023-11-19 15:48:42 -0500

    ClearUserDataAccessInterface Implementation

--- src/use_case/clear_users/ClearUserDataAccessInterface.java
+++ src/use_case/clear_users/ClearUserDataAccessInterface.java
@@ -2,5 +2,12 @@ package use_case.clear_users;
 
 // TODO Complete me
 
+import entity.User;
+
 public interface ClearUserDataAccessInterface {
+    boolean existsByName(String identifier);
+    public String getUsers();
+    public void clear();
+
+    void save(User user);
 }
