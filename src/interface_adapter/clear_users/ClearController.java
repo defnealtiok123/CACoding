fd8a5780f923029918340cabb37e6d71d3349625
defnealtiok123 <defnealtiok1983@gmail.com>
2023-11-19 12:56:44 -0500

    GitController implementation

--- src/interface_adapter/clear_users/ClearController.java
+++ src/interface_adapter/clear_users/ClearController.java
@@ -1,5 +1,18 @@
 package interface_adapter.clear_users;
 
+import use_case.clear_users.ClearInputBoundary;
+import use_case.clear_users.ClearInputData;
+
 // TODO Complete me
 public class ClearController {
+    final ClearInputBoundary userClearUseCaseInteractor;
+    public ClearController(ClearInputBoundary userClearUseCaseInteractor) {
+        this.userClearUseCaseInteractor = userClearUseCaseInteractor;
+    }
+
+    public void execute(String username) {
+        ClearInputData clearInputData = new ClearInputData();
+
+        userClearUseCaseInteractor.execute(clearInputData);
+    }
 }
