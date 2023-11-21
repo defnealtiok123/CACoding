4dae196787c142acbf1fd116e5965ea66b74ad8c
defnealtiok123 <defnealtiok1983@gmail.com>
2023-11-20 20:49:47 -0500

    ClearViewModel Implementation

--- src/interface_adapter/clear_users/ClearViewModel.java
+++ src/interface_adapter/clear_users/ClearViewModel.java
@@ -1,6 +1,37 @@
 package interface_adapter.clear_users;
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 
 // TODO Complete me
 
-public class ClearViewModel {
+public class ClearViewModel extends ViewModel {
+    public static final String CLEAR_BUTTON_LABEL = "Clear";
+
+    private ClearState state = new ClearState();
+
+    public ClearViewModel() {
+        super("clear");
+    }
+
+    public void setState(ClearState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    // This is what the Signup Presenter will call to let the ViewModel know
+    // to alert the View
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public ClearState getState() {
+        return state;
+    }
 }
