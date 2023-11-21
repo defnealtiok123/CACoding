a11740a4f454907e271d3e63480ec8d32f065274
defnealtiok123 <defnealtiok1983@gmail.com>
2023-11-20 20:48:00 -0500

    ClearInputBoundary Implementation

--- src/use_case/clear_users/ClearInputBoundary.java
+++ src/use_case/clear_users/ClearInputBoundary.java
@@ -6,4 +6,5 @@ import use_case.signup.SignupInputData;
 
 public interface ClearInputBoundary {
     void execute(ClearInputData clearInputData);
+    // Sınce we have no ınput data for thıs use case, you dont need a parameter for your execute method; voıd execute(); ıs fıne
 }
