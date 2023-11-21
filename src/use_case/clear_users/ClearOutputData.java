78b5d9d9259a846e76fd57e6c57eb64be93837dc
defnealtiok123 <defnealtiok1983@gmail.com>
2023-11-20 20:48:58 -0500

    ClearOutputData Implementation

--- src/use_case/clear_users/ClearOutputData.java
+++ src/use_case/clear_users/ClearOutputData.java
@@ -1,6 +1,20 @@
 package use_case.clear_users;
 
-// TODO Complete me
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClearOutputData {
+    private final ArrayList<String> deletedUsers;
+    // Agaın before you gove the OUPUT DATA  to the vıew, you can store ıt however you want, but the end result should be
+    // a strıng of users.
+
+    public ClearOutputData(ArrayList<String> deletedUsers) {
+        this.deletedUsers = deletedUsers;
+    }
+
+    public ArrayList<String> getDeletedUsers() {
+        return deletedUsers;
+    }
+
 }
