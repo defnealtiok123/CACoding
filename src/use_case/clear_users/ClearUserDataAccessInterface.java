bdce59533e77c652897f2966e9ff058729369220
defnealtiok123 <defnealtiok1983@gmail.com>
2023-11-20 20:49:36 -0500

    ClearUserDataAccessInterface Implementation

--- src/use_case/clear_users/ClearUserDataAccessInterface.java
+++ src/use_case/clear_users/ClearUserDataAccessInterface.java
@@ -4,10 +4,16 @@ package use_case.clear_users;
 
 import entity.User;
 
+import java.util.ArrayList;
+
 public interface ClearUserDataAccessInterface {
     boolean existsByName(String identifier);
-    public String getUsers();
+    public ArrayList<String> getUsers();
+    // Thıs ıs stored as a Strıng, but your output data stores the users ın a lıst. They should be the same I thınk
     public void clear();
 
     void save(User user);
+
+    // you don,t need a save method for thıs use case sınce we only have to delete
+    // You also dont have to declare the methods publıc because all methods ın an ınterface are publıc
 }
